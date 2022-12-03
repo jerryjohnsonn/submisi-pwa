60db79e01486572b01afcac5b5ee1fd623ca53f5
jerryjohnsonn <mail.jerryjohnson@gmail.com>
2022-12-03 21:25:18 +0700

    File yang sudah selesai semua
    Sudah selesai semua

--- e2e/Liking_Restaurants.spec.js
+++ e2e/Liking_Restaurants.spec.js
@@ -1,20 +1,29 @@
+const assert = require('assert');
+
 Feature('Liking Restaurants');
 
 Before(({ I }) => {
   I.amOnPage('/');
 });
 
-Scenario('Liking a restaurant', async ({ I }) => {
-  I.see('Restaurant List','.content_heading');
-  // pause();
-  I.seeElement('.card');
-  I.seeElement('.restaurant__title a');
-
-  const firstRestaurant = locate('.restaurant__title a').first();
-  I.click(firstRestaurant);
-  I.seeElement('#likeButtonContainer');
-
-  const likeButton = locate('#likeButtonContainer button');
-  I.click(likeButton);
+Scenario('Liking a restaurant and unliking a restaurant', async ({ I }) => {
+  I.waitForElement('#main-content');
+  I.waitForElement('#restaurants .card');
+  I.see('Restaurant List', '.content_heading');
+  const firstIndexRestaurant = locate('#restaurants .card').first();
+  const firstIndexRestaurantName = await I.grabTextFrom(firstIndexRestaurant.find('.card-text h3 a'));
+  I.click(firstIndexRestaurant.find('a'));
+  I.waitForElement('#likeButton');
+  I.click('#likeButton');
+  I.amOnPage('/#/favorite');
+  const likedRestaurantName = await I.grabTextFrom('.card-text h3 a');
+  assert.strictEqual(firstIndexRestaurantName, likedRestaurantName);
+  I.amOnPage('/#/favorite');
+  I.waitForElement('#main-content');
+  I.waitForElement('#restaurants .card');
+  const firstLikedRestaurant = locate('#restaurants .card').first();
+  I.click(firstLikedRestaurant.find('a'));
+  I.waitForElement('#likeButton');
+  I.click('#likeButton');
   I.amOnPage('/#/favorite');
 });
--- webpack.common.js
+++ webpack.common.js
@@ -3,11 +3,13 @@ const path = require('path');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
+const ImageminMozjpeg = require('imagemin-mozjpeg');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
-    // sw: path.resolve(__dirname, 'src/scripts/sw.js')
   }, 
   output: {
     filename: '[name].bundle.js',
@@ -29,21 +31,56 @@ module.exports = {
       }
     ]
   },
+  optimization: { 
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 70000,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },  
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'index.html',
       template: path.resolve(__dirname, 'src/templates/index.html'),
+      filename: 'index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/public/'),
-          to: path.resolve(__dirname, 'dist/')
+          to: path.resolve(__dirname, 'dist/'),
+          globOptions: {
+            ignore: ['**/images/heros/**'],
+          },
         }
+      ]
+    }),
+    new ImageminWebpackPlugin({
+      plugins: [
+        ImageminMozjpeg({
+          quality: 50,
+          progressive: true,
+        }),
       ]
     }),
     new WorkboxWebpackPlugin.GenerateSW({
       swDest: './sw.bundle.js',
-    })
-  ]
+    }),
+    new BundleAnalyzerPlugin(),
+  ],
 };
